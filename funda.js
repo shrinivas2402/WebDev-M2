81512e31113214ea3ec799405f460bceb7c93cd0
shrinivas2402 <84124563+shrinivas2402@users.noreply.github.com>
2022-09-25 14:33:24 +0530

    JS Fundamentals (Question 2)

---
+++ funda.js
@@ -0,0 +1,47 @@
+// initialising a constant and pushing it to h1 in html file
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hi there, this is a String Variable';
+// initializing string variable
+let name = 'Shri';
+// initializing number variable
+let numvar = 21;
+// initializing boolean variable
+let varboo = true;
+
+// initializing an array variable
+let arr = ['Shri ', 21, 2002 ]
+// initializing object variable
+let obj = document.querySelector('h2')
+
+obj.textContent = arr;
+
+// simple addition
+let a = 10;
+let b = 11;
+let c = a+b;
+
+obj2 = document.querySelector('h3')
+obj2.textContent = c;
+
+var colours = new Array('red','blue', 'green');
+
+// for loop
+for(var i = 0; i<=10; i++)
+{
+    console.log(i);
+
+}
+
+// while loop
+var j = 5;
+while(j<10)
+{
+    console.log(j);
+    j++;
+
+}
+
+
+
+
+
